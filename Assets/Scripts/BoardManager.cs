06938efd8aee4f16aaecf73d8e74793a4538326e
Jimmy Svejderud <jimmy@svejderud.se>
2018-04-21 13:35:05 +0200

    placeholder sprites w prefabs; board manager v1

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,13 +1,147 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour
 {
-    // TODO : make singleton
+    public GameObject[] wallTiles;
+    public GameObject[] floorTiles;
+    public GameObject exitTile;
+    public GameObject playerTile;
+    public GameObject[] enemyTiles;
+    public GameObject batteryTile;
+    //public GameObject door;
+
+    private Transform board;
+
+    private const int BoardWidth = 23;
+    private const int BoardHeight = 16;
 
     void Awake()
     {
+        board = new GameObject("Board").transform;
+        LoadLevel(1);
+    }
 
+    public void LoadLevel(int levelNr)
+    {
+        Debug.Log("LOAD LEVEL DATA");
+        string levelFileName = "level" + levelNr;
+        string filePath = levelFileName; /* "Levels/" +  */
+        Debug.Log(filePath);
+        var levelTextAsset = Resources.Load<TextAsset>(filePath);
+
+        Debug.Log("PARSE LEVEL DATA");
+        var levelDefinition = ParseLevel(levelTextAsset.text);
+
+        Debug.Log("BUILD LEVEL");
+        BuildLevel(levelDefinition);
+    }
+
+    private void BuildLevel(string[,] lvldef)
+    {
+        for (int r = 0; r < BoardHeight; ++r)
+        {
+            for (int c = 0; c < BoardWidth; ++c)
+            {
+                var tileType = lvldef[r, c];
+                LoadGameObject(tileType, r, c);
+            }
+        }
+    }
+
+    private void LoadGameObject(string tileType, int row, int col)
+    {
+        /*
+            0 = wall
+            1 = floor
+            P = player
+            E = enemy
+            X = eXit
+            //D = door
+            B = battery
+         */
+        if (tileType == "0")
+        {
+            var toInstantiate = wallTiles[Random.Range(0, wallTiles.Length - 1)];
+            InstantiateBoardTile(toInstantiate, CoordsToPosition(row, col));
+        }
+        else if (tileType == "1")
+        {
+            InstantiateFloorTile(row, col);
+        }
+        else if (tileType == "P")
+        {
+            InstantiateFloorTile(row, col);
+            InstantiateBoardTile(playerTile, CoordsToPosition(row, col));
+        }
+        else if (tileType == "E")
+        {
+            InstantiateFloorTile(row, col);
+            InstantiateBoardTile(enemyTiles[Random.Range(0, enemyTiles.Length - 1)], CoordsToPosition(row, col));
+        }
+        else if (tileType == "X")
+        {
+            InstantiateFloorTile(row, col);
+            InstantiateBoardTile(exitTile, CoordsToPosition(row, col));
+        }
+        else if (tileType == "B")
+        {
+            InstantiateFloorTile(row, col);
+            InstantiateBoardTile(batteryTile, CoordsToPosition(row, col));
+        }
+        else
+        {
+            Debug.LogError("Unknown tile type(" + row + "," + col + "): " + tileType);
+        }
+    }
+
+    private void InstantiateFloorTile(int row, int col)
+    {
+        var toInstantiate = floorTiles[Random.Range(0, floorTiles.Length - 1)];
+        InstantiateBoardTile(toInstantiate, CoordsToPosition(row, col));
+    }
+
+    private Vector3 CoordsToPosition(int row, int col)
+    {
+        return new Vector3(col, -row, 0f);
+    }
+
+    private void InstantiateBoardTile(GameObject prefab, Vector3 pos)
+    {
+        var obj = (GameObject)Instantiate(prefab, pos, Quaternion.identity);
+        obj.transform.parent = board;
+    }
+
+    private string[,] ParseLevel(string levelText)
+    {
+        var rows = GetRows(levelText);
+
+        Debug.Log("r length: " + rows.Length);
+        string[,] level = new string[BoardHeight, BoardWidth];
+        for (int r = 0; r < rows.Length; ++r)
+        {
+            var cols = Cut(rows[r].Trim()); // making sure we're not getting any excess
+            Debug.Log("c length: " + cols.Length);
+            for (int c = 0; c < cols.Length; ++c)
+            {
+                //Debug.Log(string.Format("r: {0}, c: {1}", r, c));
+                level[r, c] = cols[c];
+            }
+        }
+
+        return level;
+    }
+
+    private string[] GetRows(string input)
+    {
+        return input.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    private string[] Cut(string input, char separator = ' ')
+    {
+        return input.Split(new[] {separator}, StringSplitOptions.RemoveEmptyEntries);
     }
 }
---
+++ Assets/Scripts/CameraScale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScale : MonoBehaviour
+{
+    public float floatable = 100.0f;
+    //This can be PixelsPerUnit, or you can change it during runtime to alter the camera.
+
+    private void Awake()
+    {
+        GetComponent<Camera>().orthographicSize = Screen.height * gameObject.GetComponent<Camera>().rect.height / floatable / 2.0f; //- 0.1f;
+    }
+}
