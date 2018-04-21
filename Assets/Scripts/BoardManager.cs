f18169edee29c11894d2e95b93e8184968c9b8c4
Jimmy Svejderud <jimmy@svejderud.se>
2018-04-21 11:21:55 +0200

    scripts + sprites init

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    // TODO : make singleton
+
+    void Awake()
+    {
+
+    }
+}
